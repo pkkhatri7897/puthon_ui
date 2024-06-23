3eed12cc9a33a5ce32ac1122a9fd45264ccec34a
Parth Khatri <parthkhatri7897@gmail.com>
2024-06-23 16:07:59 +0530

    app.functionality.all.done.except.clear_all

--- ttk_bootstrape/dummy_test.py
+++ ttk_bootstrape/dummy_test.py
@@ -2,25 +2,157 @@
 
 ###### change labelframe bg color
 
+# import tkinter as tk
+# import ttkbootstrap as ttk
+# from ttkbootstrap.constants import *
+
+# def change_bg_color():
+#     style.configure('Custom.TLabelframe', background='#ADD8E6')  # Light Blue color
+#     label_frame.configure(style='Custom.TLabelframe')
+
+# root = ttk.Window(themename="flatly")
+
+# # Create a style object
+# style = ttk.Style()
+
+# # Create a LabelFrame
+# label_frame = ttk.LabelFrame(root, text="My Label Frame", width=200, height=100)
+# label_frame.pack(pady=20, padx=20)
+
+# # Create a Button to change the background color
+# change_color_button = ttk.Button(root, text="Change Background Color", command=change_bg_color)
+# change_color_button.pack(pady=10)
+
+# root.mainloop()
+
+
+
+####################### popup window
+
+
+
+# import tkinter as tk
+# import ttkbootstrap as ttk
+# from ttkbootstrap.constants import *
+
+
+# class MainApp(ttk.Window):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+        
+#         # Main window settings
+#         self.title("Main Window")
+#         self.geometry("800x600")
+        
+#         # Button to open the popup
+#         self.open_popup_button = ttk.Button(self, text="Open Popup", command=self.open_popup)
+#         self.open_popup_button.pack(pady=20)
+
+#     def open_popup(self):
+#         # Create the popup window
+#         self.popup = ttk.Toplevel(self)
+#         self.popup.title("Popup Window")
+#         self.popup.geometry("400x300")
+
+#         # Treeview (list box) with two columns
+#         self.treeview = ttk.Treeview(self.popup, columns=("Column 1", "Column 2"), show="headings")
+#         self.treeview.heading("Column 1", text="Column 1")
+#         self.treeview.heading("Column 2", text="Column 2")
+#         self.treeview.pack(pady=10, padx=10, expand=True, fill=tk.BOTH)
+
+#         # Entry fields
+#         self.entry1 = ttk.Entry(self.popup)
+#         self.entry2 = ttk.Entry(self.popup)
+#         self.entry1.pack(pady=5, padx=10)
+#         self.entry2.pack(pady=5, padx=10)
+
+#         # Buttons for update and cancel
+#         self.update_button = ttk.Button(self.popup, text="Update", command=self.update_list)
+#         self.cancel_button = ttk.Button(self.popup, text="Cancel", command=self.popup.destroy)
+#         self.update_button.pack(side=tk.LEFT, padx=10, pady=10)
+#         self.cancel_button.pack(side=tk.RIGHT, padx=10, pady=10)
+
+#     def update_list(self):
+#         # Get the values from the entry fields
+#         value1 = self.entry1.get()
+#         value2 = self.entry2.get()
+
+#         # Insert the values into the treeview
+#         self.treeview.insert('', 'end', values=(value1, value2))
+
+#         # Clear the entry fields
+#         self.entry1.delete(0, tk.END)
+#         self.entry2.delete(0, tk.END)
+
+
+# if __name__ == "__main__":
+#     app = MainApp(themename="minty")
+#     app.mainloop()
+
+
+############ popup with label on entry field
+
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-def change_bg_color():
-    style.configure('Custom.TLabelframe', background='#ADD8E6')  # Light Blue color
-    label_frame.configure(style='Custom.TLabelframe')
 
-root = ttk.Window(themename="flatly")
+class MainApp(ttk.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Main window settings
+        self.title("Main Window")
+        self.geometry("800x600")
+        
+        # Button to open the popup
+        self.open_popup_button = ttk.Button(self, text="Open Popup", command=self.open_popup)
+        self.open_popup_button.pack(pady=20)
 
-# Create a style object
-style = ttk.Style()
+    def open_popup(self):
+        # Create the popup window
+        self.popup = ttk.Toplevel(self)
+        self.popup.title("Popup Window")
+        self.popup.geometry("400x300")
 
-# Create a LabelFrame
-label_frame = ttk.LabelFrame(root, text="My Label Frame", width=200, height=100)
-label_frame.pack(pady=20, padx=20)
+        # Treeview (list box) with two columns
+        self.treeview = ttk.Treeview(self.popup, columns=("Column 1", "Column 2"), show="headings")
+        self.treeview.heading("Column 1", text="Column 1")
+        self.treeview.heading("Column 2", text="Column 2")
+        self.treeview.pack(pady=10, padx=10, expand=True, fill=tk.BOTH)
 
-# Create a Button to change the background color
-change_color_button = ttk.Button(root, text="Change Background Color", command=change_bg_color)
-change_color_button.pack(pady=10)
+        # Labels and Entry fields in a grid layout
+        self.label1 = ttk.Label(self.popup, text="Input 1:")
+        self.entry1 = ttk.Entry(self.popup)
+        self.label2 = ttk.Label(self.popup, text="Input 2:")
+        self.entry2 = ttk.Entry(self.popup)
+        
+        self.label1.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
+        self.entry1.grid(row=0, column=1, padx=10, pady=5, sticky=tk.EW)
+        self.label2.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+        self.entry2.grid(row=1, column=1, padx=10, pady=5, sticky=tk.EW)
 
-root.mainloop()
+        self.popup.grid_columnconfigure(1, weight=1)  # Make the second column expandable
+
+        # Buttons for update and cancel
+        self.update_button = ttk.Button(self.popup, text="Update", command=self.update_list)
+        self.cancel_button = ttk.Button(self.popup, text="Cancel", command=self.popup.destroy)
+        self.update_button.grid(row=2, column=0, padx=10, pady=10)
+        self.cancel_button.grid(row=2, column=1, padx=10, pady=10, sticky=tk.E)
+
+    def update_list(self):
+        # Get the values from the entry fields
+        value1 = self.entry1.get()
+        value2 = self.entry2.get()
+
+        # Insert the values into the treeview
+        self.treeview.insert('', 'end', values=(value1, value2))
+
+        # Clear the entry fields
+        self.entry1.delete(0, tk.END)
+        self.entry2.delete(0, tk.END)
+
+
+if __name__ == "__main__":
+    app = MainApp(themename="minty")
+    app.mainloop()
--- ttk_bootstrape/rfid_app_bootstrape.py
+++ ttk_bootstrape/rfid_app_bootstrape.py
@@ -13,6 +13,8 @@ from screeninfo import get_monitors
 import ctypes
 from ttkbootstrap.constants import *
 from ttkbootstrap.scrolled import ScrolledFrame
+import tkinter.font as tkFont
+from PIL import Image, ImageTk
 
 
 import serial
@@ -26,8 +28,51 @@ class Reader():
         combobox_font = ("Helvetica", 8)  # Adjust the font family and size as needed
         # self.style.configure('TCombobox', font=combobox_font, background="orange")
         # self.style.configure('TLabelframe.Label', font=combobox_font, background='orange')  # Set the background color
-
         
+        # Apply the default theme
+        self.default_theme = "vapor"
+        self.style.theme_use(self.default_theme)
+
+
+        ###############################################################################        
+        # CSV PATH & VARIABLE OF ACTIONS
+        ###############################################################################    
+        # VARIABLE
+        self.action_name = "Entrance"
+        self.time_duration = 5
+
+        # CSV
+        self.tags_details = []
+        self.tags_references = {}
+        # self.home_loc = os.path.expanduser('')
+        self.home_loc = os.getcwd()
+        self.orange_rfid = os.path.join(self.home_loc, "orange")
+        os.makedirs(self.orange_rfid, exist_ok=True)
+        self.tags_details_csv = os.path.join(self.orange_rfid, "tags_details.csv")
+
+        ###############################################################################  
+
+
+        ###############################################################################        
+        # LOGO
+        ###############################################################################
+
+        # Create a frame to hold the image
+        self.image_frame = ttk.Frame(self.root)
+        self.image_frame.pack(padx=20, pady=20)
+
+        # Load and resize the image
+        image_path = "orange\orange.png"  # Replace with your image path
+        original_image = Image.open(image_path)
+        resized_image = original_image.resize((200, 50), Image.LANCZOS)  # Adjust size as needed
+        self.image = ImageTk.PhotoImage(resized_image)
+
+        # Create a label to display the image
+        self.image_label = ttk.Label(self.image_frame, image=self.image)
+        self.image_label.pack()
+
+        self.image_frame.place(relx=0.02, rely=0.02)
+        ###############################################################################  
         
         
         ###############################################################################        
@@ -38,9 +83,8 @@ class Reader():
         self.theme_label_frame = tk.ttk.LabelFrame(self.theme_root, text="Themes", height=5, width=80, bootstyle="secondary")
         self.theme_label_frame.pack()
         self.theme_var = tk.StringVar()
-        self.theme_var.set("minty")
-        self.theme_combobox = ttk.Combobox(self.theme_label_frame, textvariable=self.theme_var, values=self.get_theme_list(self.style), height=3, width=15)
-        # self.theme_combobox.configure(style="TCombobox")
+        self.theme_var.set("vapor")
+        self.theme_combobox = ttk.Combobox(self.theme_label_frame, textvariable=self.theme_var, values=self.get_theme_list(self.style), height=3, width=15, state="readonly")
         self.theme_combobox.pack(pady=5, padx=5)
         self.theme_combobox.bind("<<ComboboxSelected>>", self.apply_theme)
         self.theme_root.place(relx=0.80, rely=0.02)
@@ -51,10 +95,17 @@ class Reader():
         # OPERATIONS 
         ############################################################################### 
         
-        # BUTTON FONT STYLE
-        self.opr_btn_font = ttk.Style()
-        self.opr_btn_style_type = "info.Link.TButton"
-        self.opr_btn_font.configure(self.opr_btn_style_type, font=(14))
+        # # BUTTON FONT STYLE
+        # self.opr_btn_font = ttk.Style()
+        # self.opr_btn_style_type = "info.Link.TButton"
+        # self.opr_btn_font.configure(self.opr_btn_style_type, font=(14))
+
+        # # POPUP BUTTON FONT STYLE
+        # self.pop_btn_font = ttk.Style()
+        # self.pop_btn_style_type = "success.Link.TButton"
+        # self.pop_btn_font.configure(self.opr_btn_style_type, font=(11))
+
+        self.apply_custom_styles()
 
         # OPERATION ROOT
         self.operation_root = ttk.Frame(self.root)
@@ -73,7 +124,49 @@ class Reader():
         self.clear_all.grid(row=0, column=2, padx=40, pady=5)
 
         self.operation_root.place(relx=0.15, rely=0.15)
+        ###############################################################################
+
+
         ###############################################################################        
+        # MAIN FRAME TREEVIEW
+        ###############################################################################
+        # CREATE ROOT OF MAINFRAME EXCEL
+        self.main_entry_root = ttk.Frame(self.root)
+        self.main_entry_label = tk.ttk.LabelFrame(self.main_entry_root, text="History", bootstyle="secondary", height=500, width=780)
+
+        # Treeview (list box) with two columns
+        self.treeview = ttk.Treeview(self.main_entry_label, columns=("Column 1", "Column 2", "Column 3", "Column 4"), show="headings")
+        self.treeview.heading("Column 1", text="Time_stamp")
+        self.treeview.heading("Column 2", text="Reference")
+        self.treeview.heading("Column 3", text="Tags")
+        self.treeview.heading("Column 4", text="Actions")
+
+        # Adjust column widths based on content
+        self.treeview.column("Column 1", width=200)  # Adjust width as needed
+        self.treeview.column("Column 2", width=160)
+        self.treeview.column("Column 3", width=200)
+        self.treeview.column("Column 4", width=200)
+
+        self.treeview.grid(row=0, column=1, padx=5, pady=5,  sticky="nsew")
+
+        # Configure treeview to expand with the frame
+        self.main_entry_label.grid_rowconfigure(0, weight=1)
+        self.main_entry_label.grid_columnconfigure(0, weight=1)
+        
+        self.main_entry_label.pack(padx=5, pady=5, fill='both', expand=True)
+        self.main_entry_root.place(relx=0.01, rely=0.3, relwidth=0.98, relheight=0.7)
+        ###############################################################################
+
+
+        ###############################################################################        
+        # MAIN THREAD
+        ###############################################################################
+        # self.read_console_thread = threading.Thread(target=self.read_console)
+        # self.read_console_thread.daemon = True
+        # self.read_console_thread.start()
+        ###############################################################################
+        
+                
 
     
     def get_theme_list(self, ttk_style):
@@ -84,7 +177,11 @@ class Reader():
     def apply_custom_styles(self):
         self.opr_btn_font = ttk.Style()
         self.opr_btn_style_type = "info.Link.TButton"
-        self.opr_btn_font.configure(self.opr_btn_style_type, font=(14))
+        self.opr_btn_font.configure(self.opr_btn_style_type, font=("Arial", 12))
+
+        self.pop_btn_font = ttk.Style()
+        self.pop_btn_style_type = "success.Link.TButton"
+        self.pop_btn_font.configure(self.opr_btn_style_type, font=(12))
 
 
     def apply_theme(self, event):
@@ -99,13 +196,235 @@ class Reader():
         pass
 
 
+    def show_custom_message(self, value1, value2):
+       custom_msg = ttk.Toplevel(root)
+       custom_msg.title("Custom Message")
+       custom_msg.geometry("300x200")  # Set custom size
+       # Create a custom font for the message text
+       custom_font = tkFont.Font(family="Arial", size=10)
+       # Message content
+       self.action_name = value1
+       self.time_duration = value2
+       message = f'''
+            Name : {value1},
+            Time : {value2} seconds
+       '''
+       # Label to display the message
+       msg_label = ttk.Label(custom_msg, text=message, font=custom_font)
+       msg_label.pack()
+       # Button to close the custom message box
+       close_button = ttk.Button(custom_msg, text="OK", command=custom_msg.destroy, bootstyle="success-link", style=self.pop_btn_style_type)
+       close_button.pack(pady=10)
+
+
+    def open_actions_popup(self, root, act_var, time_var):
+        # CREATE WINDOW
+        popup = ttk.Toplevel(root)
+        popup.title("Actions")
+        popup.geometry("400x200")
+
+        # Entry fields
+        entry1_label = ttk.Label(popup, text="Actions :")
+        entry1_label.grid(row=0, column=0, pady=5, padx=10, stick="e")
+        entry1 = ttk.Entry(popup, width=30)
+        entry1.insert(0, "Entrance")
+        entry1.grid(row=0, column=1, pady=5, padx=10, stick="w")
+        
+        entry2_label = ttk.Label(popup, text="Time duration :")
+        entry2_label.grid(row=1, column=0, pady=5, padx=10, stick="e")
+        entry2 = ttk.Entry(popup, width=8)
+        entry2.insert(0, 5)
+        entry2.grid(row=1, column=1, pady=5, padx=10, stick="w")
+        label3 = ttk.Label(popup, text="seconds")
+        label3.grid(row=1, column=1, padx=70, pady=10)
+
+        # CHANGE VALUES OF ACTIONS VARIABLE
+        def get_values():
+           action = entry1.get()
+           time_duration = entry2.get()
+           act_var = action
+           self.action_name = action
+           self.time_duration = time_duration
+           time_var = time_duration
+           self.show_custom_message(act_var, time_var)
+           popup.destroy()
+
+        # Button to close the popup window
+        close_button = ttk.Button(popup, text="Cancel", command=popup.destroy, bootstyle="success-link", style=self.pop_btn_style_type)
+        close_button.grid(row=2, column=1, padx=10, pady=20, sticky='e')
+        accept_button = ttk.Button(popup, text="Accept", command=get_values, bootstyle="success-link", style=self.pop_btn_style_type)
+        accept_button.grid(row=2, column=2, padx=10, pady=20)
+
+
     def actions(self):
-        pass
+        self.open_actions_popup(self.root, self.action_name, self.time_duration)
+
+    
+    def open_tag_popup(self, root, csv_path):
+        # CREATE WINDOW
+        popup = ttk.Toplevel(root)
+        popup.title("Tags")
+        popup.geometry("500x400")
+
+        # Treeview (list box) with two columns
+        treeview = ttk.Treeview(popup, columns=("Column 1", "Column 2"), show="headings")
+        treeview.heading("Column 1", text="Tags")
+        treeview.heading("Column 2", text="References")
+        treeview.grid(row=0, column=1, padx=5, pady=5)
+
+        # CSV HANDLING
+        if os.path.exists(csv_path) and os.path.getsize(csv_path) > 0:
+            df = pd.read_csv(csv_path, header=None)
+            if not df.empty:
+            # if df is not None:
+                tag_name = df[0]
+                ref_name = df[1]
+                for i, j in zip(tag_name, ref_name):
+                    treeview.insert('', 'end', values=(i, j))
+
+        # Entry fields
+        entry1_label = ttk.Label(popup, text="Tags :")
+        entry1_label.grid(row=1, column=0, pady=5, padx=10, stick="e")
+        entry1 = ttk.Entry(popup, width=30)
+        entry1.grid(row=1, column=1, pady=5, padx=10, stick="w")
+        
+        entry2_label = ttk.Label(popup, text="References :")
+        entry2_label.grid(row=2, column=0, pady=5, padx=10, stick="e")
+        entry2 = ttk.Entry(popup, width=30)
+        entry2.grid(row=2, column=1, pady=5, padx=10, stick="w")
+
+        
+        def update_list():
+            all_data = {"tags" : [], "references": []}
+            value1 = entry1.get()
+            value2 = entry2.get()
+            # Insert values into the listbox (Treeview)
+            treeview.insert('', 'end', values=(value1, value2))
+            entry1.delete(0, tk.END)
+            entry2.delete(0, tk.END)
+            all_data["tags"].append(value1)
+            all_data["references"].append(value2)
+            df = pd.DataFrame(all_data)
+            if os.path.isfile(csv_path):
+                df.to_csv(csv_path, mode='a', header=False, index=False)
+            else:
+                df.to_csv(csv_path, mode='w', header=False, index=False)
+
+        
+        def delete_selected():
+            selected_item = treeview.selection()
+            if selected_item:
+                for item in selected_item:
+                    treeview.delete(item)
+                # Update the CSV file
+                all_data = []
+                for child in treeview.get_children():
+                    all_data.append(treeview.item(child)["values"])
+                df = pd.DataFrame(all_data, columns=["tags", "references"])
+                df.to_csv(csv_path, mode='w', header=False, index=False)
+
+        
+        # BUTTON
+        delete_button = ttk.Button(popup, text="Delete Entry", command=delete_selected, bootstyle="success-link", style=self.pop_btn_style_type)
+        delete_button.grid(row=5, column=1, padx=5, pady=10)
+        
+        update_button = ttk.Button(popup, text="Update List", command=update_list, bootstyle="success-link", style=self.pop_btn_style_type)
+        update_button.grid(row=5, column=1, padx=5, pady=10, sticky='e')
+
+        close_button = ttk.Button(popup, text="Close", command=popup.destroy, bootstyle="success-link", style=self.pop_btn_style_type)
+        close_button.grid(row=5, column=2, padx=10, pady=10, sticky='w')
 
 
     def tags(self):
-        pass
+        self.open_tag_popup(self.root, self.tags_details_csv)
+    
 
+    def __check_tag_details(self, tag_id, current_time):
+        for tags in self.tags_details:
+            if (tags['tag_id'] == tag_id):
+                if (tags["last_updated_time"] > (current_time + datetime.timedelta(seconds=int(self.time_duration)))):
+                    return True
+                else:
+                    return False
+        else:
+            return "Not Found!"
+       
+    
+    def __update_tag_details(self, tag_id, current_time):
+        for tags in self.tags_details:
+            if (tags["tag_id"] == tag_id):
+                tags["last_updated_time"] = current_time
+                return True
+        else:
+            tag_details = {}
+            tag_details["tag_id"] = tag_id
+            tag_details["last_updated_time"] = current_time
+            self.tags_details.append(tag_details)
+
+
+    def __insert_into_treeview(self, time_stamp, references, tag):
+        if self.action_name is not None:
+            self.tree.insert("", tk.END, values=(time_stamp, references, tag, self.action_name))
+        else:
+            tk.messagebox.showerror("Error", "Please update action name first!")
+
+
+    def read_console(self):
+        self.reader.initialize_reader()
+        self.reader.get_device_info()
+        self.reader.get_all_params()
+        self.reader.start_inventory()
+        prev_val = 0
+        # self.reader.start_scanning()
+        try:
+            while True:
+                if self.reader.serial_port.in_waiting:
+                    response = self.reader.serial_port.read(self.reader.serial_port.in_waiting)
+                    print("Tag Read Response:", response)
+                    if response is not None:
+                        if len(response) > 26:
+                            delimiter = b'\xcf'
+                            split_bytes = response.split(delimiter)
+                            split_segments = [delimiter + segment for segment in split_bytes if segment]
+                            for i, segment in enumerate(split_segments):
+                                tag_data = segment[-14:-2]
+                                # tag_data = segment[-7:-2]
+                                # if len(tag_data) > 5:
+                                print(f"tag_data_len: {len(tag_data)}")
+                                integer_value = int.from_bytes(tag_data, byteorder='big')
+                                print(f"tag_data: {integer_value}")
+                                ts = datetime.datetime.now()
+                                ret = self.__check_tag_details(integer_value, ts)
+                                print(f"ret: {ret}")
+                                if ret==True or ret=="Not Found!":
+                                    # ts = datetime.datetime.now()
+                                    formatted_time = ts.strftime("%d/%m/%y, %H:%M")
+                                    self.__insert_into_treeview(formatted_time, "pc 98", integer_value)
+                                    self.__update_tag_details(integer_value, ts)
+                                    print(self.tags_details)
+                        self.get_data = True
+                        if response[0] != 0xCF:
+                            print("Invalid start byte")
+                        tag_data = response[-14:-2]
+                        # tag_data = response[-7:-2]
+                        # if len(tag_data) > 5:
+                        print(f"tag_data_len: {len(tag_data)}")
+                        integer_value = int.from_bytes(tag_data, byteorder='big')  # or 'little' depending on your endianness
+                        print(f"tag_data: {integer_value}")
+                        ts = datetime.datetime.now()
+                        ret = self.__check_tag_details(integer_value, ts)
+                        print(f"ret: {ret}")
+                        if ret==True or ret=="Not Found!":
+                            # ts = datetime.datetime.now()
+                            formatted_time = ts.strftime("%d/%m/%y, %H:%M")
+                            self.__insert_into_treeview(formatted_time, "pc 98", integer_value)
+                            self.__update_tag_details(integer_value, ts)
+                            print(self.tags_details)
+                time.sleep(0.1)
+                # print(self.tags_details)
+        except KeyboardInterrupt:
+            self.reader.stop_inventory(self.reader.serial_port)
+            print("Stopped inventory")
 
 if __name__=="__main__":
     screen_resolution = [(m.width, m.height) for m in get_monitors()]
